f344a4dd4b2b295f3840b6bcdb13478cd1c95d32
AlexStav <astavrianos@blastup.digital>
2021-07-21 19:33:29 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope module: 'weather_checker' do
+    resources :cities, only: [:index, :update]
+    resources :temperatures, only: [:index, :create]
+
     root to: 'home#index'
   end
 end
