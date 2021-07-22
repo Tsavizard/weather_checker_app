4daad57a1a38479dac3c432a351a737a7795bf31
AlexStav <astavrianos@blastup.digital>
2021-07-22 09:55:34 +0300

    Update Home.js

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -12,21 +12,59 @@ class Home extends React.Component {
   }
 
   componentDidMount(){
-    //Fetch temperatures
+    this.checkTemperatures()
   }
 
   checkTemperatures(){
+    //Fetch temperatures
+  }
 
+  // addCity(cityName) {
+  //   //Update request for city. If success then call toggleCity
+  //   toggleCity(cityName)
+  // }
+
+  // removeCity(cityName) {
+  //   //Update request for city. If success then call toggleCity
+  //   toggleCity(cityName)
+  // }
+
+  toggleCity(cityName) {
+    this.setState(prevState => {
+      let newCities = [...prevState.cities]
+      const index = newCities.findIndex(c => c.name == cityName)
+      newCities[index].is_included = !newCities[index].is_included
+
+      return({
+        cities: newCities
+      })
+    })
   }
 
   render () {
     return (
       <React.Fragment>
         <div>
-          Cities: {this.props.cities}
+          <h1>Welcome to Weather Checker</h1>
         </div>
-        <div>
-          {/* Temperature Fetch Path: {this.props.temperaturePath} */}
+
+        <div className='row'>
+          <span >Cities: </span>
+          {
+            this.state.cities.map((c,i) => {
+              return(
+                <div className='col s12' key={i}>
+                  { c.name }
+                  <AddRemoveCityButton
+                    city={c}
+                    clickHandler={() => { this.toggleCity(c.name)
+                      // c.is_included ? this.addCity(c.name) : this.removeCity(c.name)
+                    } }
+                />
+                </div>
+              )
+            })
+          }
         </div>
       </React.Fragment>
     );
@@ -37,4 +75,12 @@ Home.propTypes = {
   cities: PropTypes.array,
   temperaturePath: PropTypes.string
 };
+
+function AddRemoveCityButton({city, clickHandler}) {
+  const sign = city.is_included ? '-' : '+'
+  return(
+    <button type='button' onClick={clickHandler}>{sign}</button>
+  )
+}
+
 export default Home
