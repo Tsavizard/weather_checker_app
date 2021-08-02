a9f94f509103c1d52c28cb91bb259a49db413ab0
AlexStav <astavrianos@blastup.digital>
2021-08-02 21:55:24 +0300

    fixes

--- app/services/create_temperature.rb
+++ app/services/create_temperature.rb
@@ -2,7 +2,7 @@ class CreateTemperature
 
   def self.call_for_cities(city_names)
     cities = WeatherChecker::City.included.where(name: city_names)
-    checker_api = Api::TemperatureFetcher.new
+    checker_api = TemperatureFetcher.new
 
     results = cities.map do |city|
       response = checker_api.get_weather_for_city(city.name)
@@ -13,15 +13,15 @@ class CreateTemperature
                                            .select('MIN(temperatures.value) as min_temp, MAX(temperatures.value) as max_temp, AVG(temperatures.value) as avg_temp')
                                            .first
       reading[city.name] = {
-        min: temperature_aggregations['min_temp'],
-        max: temperature_aggregations['max_temp'],
-        avg: temperature_aggregations['avg_temp'],
+        min: city_temperatures['min_temp'],
+        max: city_temperatures['max_temp'],
+        avg: city_temperatures['avg_temp'],
         last: temperature.value
       }
       reading
     end
 
-  rescue
+  rescue => e
     { error: 'Error connecting to API', status_code: 500 }
   end
 end
