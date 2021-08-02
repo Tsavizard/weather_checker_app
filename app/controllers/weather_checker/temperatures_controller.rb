dd24d43f940af710f97ced055a43ba2dddc757af
AlexStav <astavrianos@blastup.digital>
2021-08-02 21:45:34 +0300

    updated routes

--- app/controllers/weather_checker/temperatures_controller.rb
+++ app/controllers/weather_checker/temperatures_controller.rb
@@ -1,7 +1,7 @@
 module WeatherChecker
   class TemperaturesController < ApplicationController
     def check_all
-      city_names = Array.wrap(params[:city_names])
+      city_names = Array.wrap(params[:city_names]&.split(','))
       results = ::CreateTemperature.call_for_cities(city_names)
 
       respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope module: 'weather_checker' do
-    resources :cities, only: [:create, :destroy], key: :city_name
+    resources :cities, only: [:create, :destroy], param: :city_name
     get 'temperatures', to: 'temperatures#check_all'
 
     root to: 'home#index'
