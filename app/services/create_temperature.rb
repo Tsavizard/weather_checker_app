8903b8bd40a6f7c470159f65615e961d3fc7b4c8
AlexStav <astavrianos@blastup.digital>
2021-08-03 12:02:05 +0300

    Update create_temperature.rb

--- app/services/create_temperature.rb
+++ app/services/create_temperature.rb
@@ -3,15 +3,16 @@ class CreateTemperature
   def self.call_for_cities(city_names)
     cities = WeatherChecker::City.included.where(name: city_names)
     checker_api = TemperatureFetcher.new
+    query = 'MIN(temperatures.value) as min_temp, MAX(temperatures.value) as max_temp, AVG(temperatures.value) as avg_temp'
 
     results = cities.map do |city|
       response = checker_api.get_weather_for_city(city.name)
       temperature = city.temperatures.create(value: response['main']['temp'])
 
       reading = {}
-      city_temperatures = city.temperatures.reload
-                                           .select('MIN(temperatures.value) as min_temp, MAX(temperatures.value) as max_temp, AVG(temperatures.value) as avg_temp')
-                                           .first
+      city_temperatures = city.temperatures
+                              .select(query)
+                              .first
       reading[city.name] = {
         min: city_temperatures['min_temp'],
         max: city_temperatures['max_temp'],
