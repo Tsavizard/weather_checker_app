c03566a7c5f2cb5aa2c6ca15abfde527068f01a3
AlexStav <astavrianos@blastup.digital>
2021-08-03 11:47:06 +0300

    nothing

--- app/controllers/weather_checker/home_controller.rb
+++ app/controllers/weather_checker/home_controller.rb
@@ -1,6 +1,7 @@
 module WeatherChecker
   class HomeController < ApplicationController
     def index
+      @serialized_cities = WeatherChecker::City.all.map{|city| WeatherChecker::CitySerializer.new(city).as_json}
     end
   end
 end
